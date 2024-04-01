7cbc8cb8089359ed68adf4efd9c84b2262e98ce7
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-04-01 19:03:25 -0400

    Typo

--- src/components/global/subscription-modal.tsx
+++ src/components/global/subscription-modal.tsx
@@ -12,12 +12,12 @@ import { useSupabaseUser } from '@/lib/providers/supabase-user-provider';
 import { formatPrice, postData } from '@/lib/utils';
 import { Button } from '../ui/button';
 import Loader from './Loader';
-import { Price, ProductWirhPrice } from '@/lib/supabase/supabase.types';
+import { Price, ProductWithPrice } from '@/lib/supabase/supabase.types';
 import { useToast } from '../ui/use-toast';
 import { getStripe } from '@/lib/stripe/stripeClient';
 
 interface SubscriptionModalProps {
-  products: ProductWirhPrice[];
+  products: ProductWithPrice[];
 }
 
 const SubscriptionModal: React.FC<SubscriptionModalProps> = ({ products }) => {
